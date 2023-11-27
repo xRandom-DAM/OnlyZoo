e94d3853a18ce3d53abfebc27098f88cafabe338
manuelmsni <manuelmsni@hotmail.com>
2023-11-27 17:12:14 +0100

    Creada interfaz DAO

---
+++ OnlyZoo/OnlyZoo/DAO/DAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.DAO
+{
+    public interface DAO<T> where T : class
+    {
+        public List<T> getAll(Object obj);
+        public T selectObject(String uuid);
+        public T insertObject(T obj);
+        public T updateObject(T obj);
+    }
+}
