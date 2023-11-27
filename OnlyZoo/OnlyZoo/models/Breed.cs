f0be8610ef271e52f70d311e45127aa830900921
manuelmsni <manuelmsni@hotmail.com>
2023-11-27 17:51:40 +0100

    Creadas clases Pet, Breed y Tag

---
+++ OnlyZoo/OnlyZoo/models/Breed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.models
+{
+    public class Breed
+    {
+        public Guid Id { get; private set; }
+        public string Species { get; set; }
+        public string Kind { get; set; }
+        public Breed(Guid id, string species, string kind)
+        {
+            Id = id;
+            Species = species;
+            Kind = kind;
+        }
+        public Breed(string species, string kind)
+        {
+            Id = Guid.NewGuid();
+            Species = species;
+            Kind = kind;
+        }
+        public Breed(){}
+    }
+}
---
+++ OnlyZoo/OnlyZoo/models/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.models
+{
+    public class Pet
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid Breed { get; set; }
+        public Pet(Guid id, string name, string description, Guid breed) {
+            Id = id;
+            Name = name;
+            Description = description;
+            Breed = breed;
+        }
+        public Pet(string name, string description, Guid breed)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Breed = breed;
+        }
+        public Pet() { }
+    }
+}
