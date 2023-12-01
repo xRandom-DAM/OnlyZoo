a677d7382085fb9fb997033db7a2d176903d38f4
Santi <14santi3c@gmail.com>
2023-12-01 13:21:49 +0100

    PetView pull-request 1

---
+++ OnlyZoo/components/PetButton.cs
@@ -0,0 +1,39 @@
+﻿using OnlyZoo.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.components
+{
+    internal class PetButton : Button
+    {
+        public PetButton(Pet pet) 
+        {
+            string nombreBtn = "Id_" + pet.Id;
+            Text = pet.Name;
+            Name = nombreBtn;
+            TextAlign = ContentAlignment.BottomCenter;
+            
+            Click += new System.EventHandler(this.MostrarPet);
+        }
+
+        /// <summary>
+        /// A partir de un String que contiene el Id de una mascota 
+        /// desarrollar "Interfaz de la mascota" Tarea 11
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MostrarPet(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string pet_ID = button.Name; //Example --> "Id_12"
+                MessageBox.Show("Muestra Contenido sobre la mascota elegida: " + pet_ID);
+
+            }
+        }
+    }
+
+}
