deee5b14a21e50e798bd1c1c1c9a34d05eb49da2
Santi <14santi3c@gmail.com>
2023-12-04 21:49:18 +0100

    MascotasVista fixed

---
+++ OnlyZoo/Mascotas.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace OnlyZoo
+{
+    partial class Mascotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblShowID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelPet = new System.Windows.Forms.Panel();
+            this.lblShowDescription = new System.Windows.Forms.Label();
+            this.lblShowName = new System.Windows.Forms.Label();
+            this.lblShowKind = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblShowSpecies = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.flowLayoutPanelTags = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelTag = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelBreed = new System.Windows.Forms.Label();
+            this.pictureBoxPet = new System.Windows.Forms.PictureBox();
+            this.panelTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelPet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(97)))), ((int)(((byte)(112)))));
+            this.panelTop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelTop.Controls.Add(this.panel1);
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(734, 71);
+            this.panelTop.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblShowID);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(619, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(115, 71);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblShowID
+            // 
+            this.lblShowID.AutoSize = true;
+            this.lblShowID.Location = new System.Drawing.Point(37, 44);
+            this.lblShowID.Name = "lblShowID";
+            this.lblShowID.Size = new System.Drawing.Size(45, 15);
+            this.lblShowID.TabIndex = 6;
+            this.lblShowID.Text = "\"ID_69\"";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(190)))), ((int)(((byte)(176)))));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pet ID:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(45, 24);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(111, 20);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Titulo: OnlyZoo";
+            // 
+            // panelPet
+            // 
+            this.panelPet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(245)))), ((int)(((byte)(240)))));
+            this.panelPet.Controls.Add(this.lblShowDescription);
+            this.panelPet.Controls.Add(this.lblShowName);
+            this.panelPet.Controls.Add(this.lblShowKind);
+            this.panelPet.Controls.Add(this.label6);
+            this.panelPet.Controls.Add(this.lblShowSpecies);
+            this.panelPet.Controls.Add(this.label4);
+            this.panelPet.Controls.Add(this.flowLayoutPanelTags);
+            this.panelPet.Controls.Add(this.labelTag);
+            this.panelPet.Controls.Add(this.labelDescription);
+            this.panelPet.Controls.Add(this.labelName);
+            this.panelPet.Controls.Add(this.labelBreed);
+            this.panelPet.Controls.Add(this.pictureBoxPet);
+            this.panelPet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPet.Location = new System.Drawing.Point(0, 71);
+            this.panelPet.Name = "panelPet";
+            this.panelPet.Size = new System.Drawing.Size(734, 340);
+            this.panelPet.TabIndex = 4;
+            // 
+            // lblShowDescription
+            // 
+            this.lblShowDescription.AutoSize = true;
+            this.lblShowDescription.Location = new System.Drawing.Point(307, 174);
+            this.lblShowDescription.Name = "lblShowDescription";
+            this.lblShowDescription.Size = new System.Drawing.Size(38, 15);
+            this.lblShowDescription.TabIndex = 11;
+            this.lblShowDescription.Text = "label7";
+            // 
+            // lblShowName
+            // 
+            this.lblShowName.AutoSize = true;
+            this.lblShowName.Location = new System.Drawing.Point(486, 118);
+            this.lblShowName.Name = "lblShowName";
+            this.lblShowName.Size = new System.Drawing.Size(38, 15);
+            this.lblShowName.TabIndex = 10;
+            this.lblShowName.Text = "label5";
+            // 
+            // lblShowKind
+            // 
+            this.lblShowKind.AutoSize = true;
+            this.lblShowKind.Location = new System.Drawing.Point(619, 57);
+            this.lblShowKind.Name = "lblShowKind";
+            this.lblShowKind.Size = new System.Drawing.Size(38, 15);
+            this.lblShowKind.TabIndex = 9;
+            this.lblShowKind.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(575, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Kind:";
+            // 
+            // lblShowSpecies
+            // 
+            this.lblShowSpecies.AutoSize = true;
+            this.lblShowSpecies.Location = new System.Drawing.Point(415, 57);
+            this.lblShowSpecies.Name = "lblShowSpecies";
+            this.lblShowSpecies.Size = new System.Drawing.Size(38, 15);
+            this.lblShowSpecies.TabIndex = 7;
+            this.lblShowSpecies.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Species:";
+            // 
+            // flowLayoutPanelTags
+            // 
+            this.flowLayoutPanelTags.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanelTags.Location = new System.Drawing.Point(307, 261);
+            this.flowLayoutPanelTags.Name = "flowLayoutPanelTags";
+            this.flowLayoutPanelTags.Size = new System.Drawing.Size(399, 70);
+            this.flowLayoutPanelTags.TabIndex = 5;
+            // 
+            // labelTag
+            // 
+            this.labelTag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(190)))), ((int)(((byte)(176)))));
+            this.labelTag.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTag.Location = new System.Drawing.Point(278, 226);
+            this.labelTag.Name = "labelTag";
+            this.labelTag.Size = new System.Drawing.Size(456, 23);
+            this.labelTag.TabIndex = 4;
+            this.labelTag.Text = "Tags:";
+            this.labelTag.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(190)))), ((int)(((byte)(176)))));
+            this.labelDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDescription.Location = new System.Drawing.Point(278, 142);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(456, 23);
+            this.labelDescription.TabIndex = 3;
+            this.labelDescription.Text = "Description:";
+            this.labelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelName
+            // 
+            this.labelName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(190)))), ((int)(((byte)(176)))));
+            this.labelName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelName.Location = new System.Drawing.Point(278, 84);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(456, 23);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Name:";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBreed
+            // 
+            this.labelBreed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(190)))), ((int)(((byte)(176)))));
+            this.labelBreed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelBreed.Location = new System.Drawing.Point(278, 23);
+            this.labelBreed.Name = "labelBreed";
+            this.labelBreed.Size = new System.Drawing.Size(456, 23);
+            this.labelBreed.TabIndex = 1;
+            this.labelBreed.Text = "Breed:";
+            this.labelBreed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBoxPet
+            // 
+            this.pictureBoxPet.BackColor = System.Drawing.Color.White;
+            this.pictureBoxPet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxPet.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxPet.Name = "pictureBoxPet";
+            this.pictureBoxPet.Size = new System.Drawing.Size(279, 340);
+            this.pictureBoxPet.TabIndex = 0;
+            this.pictureBoxPet.TabStop = false;
+            // 
+            // Mascotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 411);
+            this.Controls.Add(this.panelPet);
+            this.Controls.Add(this.panelTop);
+            this.Name = "Mascotas";
+            this.Text = "Mascota";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelPet.ResumeLayout(false);
+            this.panelPet.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Label lblTitle;
+        private Panel panelTop;
+        private Panel panelPet;
+        private PictureBox pictureBoxPet;
+        private Label labelDescription;
+        private Label labelName;
+        private Label labelBreed;
+        private FlowLayoutPanel flowLayoutPanelTags;
+        private Label labelTag;
+        private Panel panel1;
+        private Label lblShowID;
+        private Label label2;
+        private Label lblShowKind;
+        private Label label6;
+        private Label lblShowSpecies;
+        private Label label4;
+        private Label lblShowDescription;
+        private Label lblShowName;
+    }
+}
