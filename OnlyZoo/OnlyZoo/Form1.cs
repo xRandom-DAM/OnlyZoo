011539b94a7fecb15e74609101a1ada42e4f91bd
manuelmsni <manuelmsni@hotmail.com>
2023-12-06 22:56:55 +0100

    Ahora la vista debería de crear un botón por cada mascota que le llegue de la base de datos, no puedo probarlo porque docker no funciona en el pc de mi casa

--- OnlyZoo/OnlyZoo/Form1.cs
+++ OnlyZoo/OnlyZoo/Form1.cs
@@ -1,4 +1,6 @@
+using OnlyZoo.components;
 using OnlyZoo.models;
+using OnlyZoo.util;
 
 namespace OnlyZoo
 {
@@ -8,7 +10,13 @@ namespace OnlyZoo
         public Form1()
         {
             InitializeComponent();
+            InitPetButtons();
             InitializeEvents();
+        }
+
+        private void InitPetButtons()
+        {
+            DataBuffer.GetInstance().PetCollection.ForEach(pet => this.flowLayoutPanel_Mascotas.Controls.Add(new PetButton(pet)));
         }
 
         /// <summary>
@@ -16,42 +24,8 @@ namespace OnlyZoo
         /// </summary>
         private void InitializeEvents()
         {
-            //create_BtnPet_InFlowLayout();  //Añadir por parámetros una lista Pet
+                      
         }
 
-        /// <summary>
-        /// Metodo que genera Botones a partir de una Lista que contiene Clase Pet.
-        /// </summary>
-        /// <param name="lista"></param>
-        private void create_BtnPet_InFlowLayout(List<Pet> lista)
-        {
-            Button button;
-            foreach (Pet pet in lista)
-            {
-                string nombreBtn = "Id_" + pet.Id;
-                button = new Button();
-                button.Text = pet.Name;
-                button.Name = nombreBtn;
-                button.TextAlign = ContentAlignment.BottomCenter;
-                this.flowLayoutPanel_Mascotas.Controls.Add(button);
-                button.Click += new System.EventHandler(this.MostrarPet);
-            }
-        }
-
-        /// <summary>
-        /// A partir de un String que contiene el Id de una mascota 
-        /// desarrollar "Interfaz de la mascota" Tarea 11
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void MostrarPet(object sender, EventArgs e)
-        {
-            if (sender is Button button)
-            {
-                string pet_ID = button.Name; //Example --> "Id_12"
-                MessageBox.Show("Muestra Contenido sobre la mascota elegida: " + pet_ID);
-                
-            }
-        }
     }
 }
